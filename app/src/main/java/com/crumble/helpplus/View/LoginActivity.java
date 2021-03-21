082bf5c21f9b9d27f689383add594316657f6aa4
Timon <vladtdr1@yahoo.com>
2021-03-21 18:53:17 +0200

    fixed architecture implementation

--- app/src/main/java/com/crumble/helpplus/View/LoginActivity.java
+++ app/src/main/java/com/crumble/helpplus/View/LoginActivity.java
@@ -1,32 +1,24 @@
 package com.crumble.helpplus.View;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.StringRes;
-import androidx.annotation.XmlRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.text.Editable;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.crumble.helpplus.Controller.LoginController;
 import com.crumble.helpplus.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText loginUsernameField;
-    private EditText loginPasswordField;
-    private TextView loginResponseText;
-    private FirebaseAuth mAuth;
+    public EditText loginUsernameField;
+    public EditText loginPasswordField;
+    public TextView loginResponseText;
+    public FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,44 +42,8 @@ public class LoginActivity extends AppCompatActivity {
             //go to home activity
         }
     }
-
-    void updateUI(FirebaseUser user)
+    public void loginAction(View view)
     {
-        loginResponseText.setVisibility(View.VISIBLE);
-        if(user!=null)
-            loginResponseText.setText("logged in successfully as " + user.getEmail());
-        else
-            loginResponseText.setText("login fail");
+        LoginController.loginAction(this,view);
     }
-
-    public void loginAction(View view){
-
-        loginUsernameField=(EditText)findViewById(R.id.loginUsernameField);
-        loginPasswordField=(EditText)findViewById(R.id.loginPasswordField);
-        loginResponseText=(TextView)findViewById(R.id.loginResponseText);
-        Editable email= loginUsernameField.getText();
-        Editable password= loginPasswordField.getText();
-
-        if(!email.toString().isEmpty()&&!password.toString().isEmpty())
-            mAuth.signInWithEmailAndPassword(email.toString(), password.toString())
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                // Sign in success, update UI with the signed-in user's information
-                                Log.d("login", "signInWithEmail:success");
-                                FirebaseUser user = mAuth.getCurrentUser();
-                                updateUI(user);
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Log.w("login", "signInWithEmail:failure", task.getException());
-                                Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                        Toast.LENGTH_SHORT).show();
-                                updateUI(null);
-                            }
-                        }
-                    });
-        else updateUI(null);
-    }
-
 }
