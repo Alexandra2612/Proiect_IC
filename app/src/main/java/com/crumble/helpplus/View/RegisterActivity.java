9404f4a909ce86adc27f23ad2ad81412f4945031
Alexandra2612 <alexandra.hertanu@yahoo.com>
2021-03-21 22:47:21 +0200

    Added intent for new activity and created necessary files

---
+++ app/src/main/java/com/crumble/helpplus/View/RegisterActivity.java
@@ -0,0 +1,20 @@
+package com.crumble.helpplus.View;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.crumble.helpplus.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+}
