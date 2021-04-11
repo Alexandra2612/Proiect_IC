7a525053be8bcf0f58d47ce19ced377e744dce7b
Alexandra2612 <alexandra.hertanu@yahoo.com>
2021-04-11 18:17:34 +0300

    Finished functionality for profile picture and nickname-waiting for database

--- app/src/main/java/com/crumble/helpplus/View/NicknameActivity.java
+++ app/src/main/java/com/crumble/helpplus/View/NicknameActivity.java
@@ -2,10 +2,13 @@ package com.crumble.helpplus.View;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.crumble.helpplus.Controller.NicknameController;
 import com.crumble.helpplus.R;
 
 public class NicknameActivity extends AppCompatActivity {
@@ -27,5 +30,11 @@ public class NicknameActivity extends AppCompatActivity {
     {
         Intent intent=new Intent(this,ProfileActivity.class);
         startActivity(intent);
+    }
+    public void changeNickname(View view)
+    {
+        EditText nicknameField =(EditText) findViewById(R.id.nicknameText);
+        Editable nickname= nicknameField.getText();
+        NicknameController.changeNickname(nickname.toString());
     }
 }
