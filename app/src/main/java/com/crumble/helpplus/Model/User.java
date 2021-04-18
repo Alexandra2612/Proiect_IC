26ffa7288448b78c3db1ffae3d11a94776b36a75
Timon <vladtdr1@yahoo.com>
2021-04-18 21:22:47 +0300

    implemented profile activity database connection functionality

--- app/src/main/java/com/crumble/helpplus/Model/User.java
+++ app/src/main/java/com/crumble/helpplus/Model/User.java
@@ -1,4 +1,60 @@
 package com.crumble.helpplus.Model;
 
 public class User {
+
+    private int id;
+    private String email;
+    private String nickname;
+    private String image;
+    private double averageGrade;
+
+    public User(int id, String email, String nickname, String image, float averageGrade) {
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+        this.image = image;
+        this.averageGrade = averageGrade;
+    }
+
+    public User() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getAverageGrade() {
+        return averageGrade;
+    }
+
+    public void setAverageGrade(double averageGrade) {
+        this.averageGrade = averageGrade;
+    }
 }
--- app/src/main/java/com/crumble/helpplus/View/ProfileActivity.java
+++ app/src/main/java/com/crumble/helpplus/View/ProfileActivity.java
@@ -1,7 +1,9 @@
 package com.crumble.helpplus.View;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -9,26 +11,85 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.crumble.helpplus.Model.User;
 import com.crumble.helpplus.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 public class ProfileActivity extends AppCompatActivity {
-    private FirebaseUser connectedUser;
+    private FirebaseUser firebaseConnectedUser;
     private ImageView profileIcon;
     private TextView usernameText;
+    private TextView gradeText;
+    private TextView idText;
 
+    private User connectedUser= new User();
+    public void setConnectedUserData()
+    {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://86.123.241.117/?action=get&object=userdatabyemail&email="+firebaseConnectedUser.getEmail();
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET, url,
+                null,
+                response -> {
+                    try{
+                        for(int i=0;i<response.length();i++){
+                            JSONObject user = response.getJSONObject(i);
+                            connectedUser.setId(user.getInt("id"));
+                            connectedUser.setEmail(user.getString("email"));
+                            connectedUser.setNickname(user.getString("nickname"));
+                            connectedUser.setImage(user.getString("image"));
+                            connectedUser.setAverageGrade(user.getDouble("averagegrade"));
+                            usernameText.setText(connectedUser.getNickname());
+                            gradeText.setText(gradeText.getText()+" "+(((Double)connectedUser.getAverageGrade()).toString()));
+                            idText.setText(idText.getText()+" "+(((Integer)connectedUser.getId()).toString())+"  ");
+                        }
+                    }catch (JSONException e){
+                        e.printStackTrace();
+                    }
+                },
+                error -> Log.e("Volley",error.getMessage()));
+        queue.add(jsonArrayRequest);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        handleSSLHandshake();
+        firebaseConnectedUser = LoginActivity.connectedUser;
+        setConnectedUserData();
         setContentView(R.layout.activity_profile);
-        connectedUser = LoginActivity.connectedUser;
         profileIcon=(ImageView)this.findViewById(R.id.profileIcon);
         profileIcon.setImageResource(R.drawable.profile_base);
         usernameText=(TextView)this.findViewById(R.id.usernameText);
-        usernameText.setText(connectedUser.getEmail());
+        gradeText=(TextView)this.findViewById(R.id.gradeText);
+        idText=(TextView)this.findViewById(R.id.idText);
+        usernameText.setText("");
+
+
+
     }
 
     @Override
@@ -37,6 +98,36 @@ public class ProfileActivity extends AppCompatActivity {
 
         //update UI
 
+    }
+
+    @SuppressLint("TrulyRandom")
+    public static void handleSSLHandshake() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }};
+
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String arg0, SSLSession arg1) {
+                    return true;
+                }
+            });
+        } catch (Exception ignored) {
+        }
     }
 
 
